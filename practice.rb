590044cb4a8522c6f2ff7cd95b96ce5bcb30ce9c
Priya Kumar <priya.kumar08@yahoo.com>
2018-07-23 18:25:17 +0000

    automatically backed up by learn

--- practice.rb
+++ practice.rb
@@ -2,6 +2,6 @@ puts "Hello World!"
 puts "I am learning to code"
 puts "at Kode with Klossy!"
 
-print "Hello World!"
-print "I am learning to code"
+print "Hello World! "
+print "I am learning to code "
 print "at Kode with KLossy!"
